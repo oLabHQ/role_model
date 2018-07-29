cbaece9d21332877617d38ce43aa688f49fc1f64
Eric Man <meric.au@gmail.com>
2018-07-30 07:29:24 +1000

    Deserializing each instance is too slow; simplify demo api.

--- src/server/demo/schema.py
+++ src/server/demo/schema.py
@@ -1,40 +1,25 @@
 import graphene
+import json
 
 from django.apps import apps
 from django.conf import settings
+from django.core import serializers
+from django.db import connection
 from graphene_django import DjangoObjectType
 
 from role_model.models import Deliverable as DeliverableModel
 from history.models import History as HistoryModel
 
 
-class Instance(graphene.Interface):
+class Instance(graphene.ObjectType):
     pk = graphene.String()
     model = graphene.String()
-
-
-history_model_object_types = {}
-
-for model_name in settings.HISTORY_MODELS:
-    model = apps.get_model(model_name)
-
-    class Meta:
-        interfaces = (Instance, graphene.Node)
-        model = model
-        name = model._meta.model_name.title()
-
-    object_type = type(
-        model._meta.model_name.title(),
-        (DjangoObjectType,),
-        {'Meta': Meta})
-
-    history_model_object_types[model_name.lower()] = object_type
-
+    fields = graphene.JSONString()
 
 
 class History(DjangoObjectType):
     pk = graphene.String()
-    fields = graphene.JSONString()
+    changes = graphene.JSONString()
     event = graphene.String()
     instance = graphene.Field(Instance)
 
@@ -43,19 +28,16 @@ class History(DjangoObjectType):
         interfaces = (graphene.Node, )
 
     def resolve_changes(self, info):
-        return dict(self.changes())
+        return dict(self.changes()) or None
 
     def resolve_event(self, info):
         return self.modification
 
     def resolve_instance(self, info):
-        data = self.serialized_data
-        model_name = data['model']
-        object_type = history_model_object_types[model_name]
-        return object_type(
-            pk=data['pk'],
-            model=model_name,
-            **self.serialized_data['fields'])
+        return Instance(
+            pk=self.serialized_data['pk'],
+            model=self.serialized_data['model'],
+            fields=self.serialized_data['fields'])
 
 
 class Query(graphene.ObjectType):
@@ -68,5 +50,4 @@ class Query(graphene.ObjectType):
         return deliverable.history().all()
 
 
-schema = graphene.Schema(query=Query, types=[History, Instance] +
-                         list(history_model_object_types.values()))
+schema = graphene.Schema(query=Query, types=[History, Instance])

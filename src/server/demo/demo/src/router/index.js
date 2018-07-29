cae6d40c43ebe1d9d93171275dd716a0ed147acc
Eric Man <meric.au@gmail.com>
2018-07-29 18:50:58 +1000

    Rename Graph.vue to Index.vue

--- src/server/demo/demo/src/router/index.js
+++ src/server/demo/demo/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Graph from '@/components/Graph'
+import Index from '@/components/Index'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -11,8 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Graph',
-      component: Graph
+      name: 'Index',
+      component: Index
     }
   ]
 })
